75db1155a108da5d96327571b0a183cd74e658e4
taehui <taehui@neuro-cle.com>
2024-02-07 14:52:24 +0900

    on off pred and proposal

--- detectron2/utils/video_visualizer.py
+++ detectron2/utils/video_visualizer.py
@@ -75,7 +75,8 @@ class VideoVisualizer:
             return frame_visualizer.output
 
         boxes = predictions.pred_boxes.tensor.numpy() if predictions.has("pred_boxes") else None
-        # boxes = predictions.proposal_boxes.tensor.numpy() if predictions.has("proposal_boxes") else None
+        if boxes is None:
+            boxes = predictions.proposal_boxes.tensor.numpy() if predictions.has("proposal_boxes") else None
         scores = predictions.scores if predictions.has("scores") else None
         classes = predictions.pred_classes.numpy() if predictions.has("pred_classes") else None
         keypoints = predictions.pred_keypoints if predictions.has("pred_keypoints") else None
