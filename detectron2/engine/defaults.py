fix masking aug

detectron2/engine/defaults.py
@@ -329,8 +329,8 @@ class DefaultPredictor:
                 # whether the model expects BGR inputs or RGB
                 original_image = original_image[:, :, ::-1]
             height, width = original_image.shape[:2]
-            image = self.aug.get_transform(original_image).apply_image(original_image)
-            image = torch.as_tensor(image.astype("float32").transpose(2, 0, 1))
+            image_np = self.aug.get_transform(original_image).apply_image(original_image)
+            image = torch.as_tensor(image_np.astype("float32").transpose(2, 0, 1))
             image.to(self.cfg.MODEL.DEVICE)
         
             scaler = image.shape[1] / height